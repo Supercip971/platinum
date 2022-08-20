d86b5f7aab8ebe6b2e1a6cabac86d80733a827fe
Supercip971 <supercyp971@gmail.com>
2022-08-20 21:50:52 +0000

    sample: added hello_world

--- sample/hello_world/main.cc
+++ sample/hello_world/main.cc
@@ -1,8 +1,13 @@
 #include <fmt/core.h>
 #include <iostream>
+#include <platinum/log/log.hpp>
 
 int main(int argc, char **argv)
 {
-    fmt::print("hello world !");
+    debug$("hello world {} {}!", 1, 2);
+    info$("info");
+    warn$("warn");
+    error$("error");
+    fatal$("fatal");
     return 0;
 }
