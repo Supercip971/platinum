5fff97508acdacd98fe91b3ac6acd133bb27cd88
Supercip971 <supercyp971@gmail.com>
2022-08-20 20:43:14 +0000

    added fmt

--- sample/hello_world/main.cc
+++ sample/hello_world/main.cc
@@ -1,6 +1,8 @@
+#include <fmt/core.h>
 #include <iostream>
 
-int main(int argc, char **argv) {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+int main(int argc, char **argv)
+{
+    fmt::print("hello world !");
+    return 0;
 }
